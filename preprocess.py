876bb692dd7ad585c5d30e447bc99fe907c5bd2e
Beyond555 <1956308821@qq.com>
2024-04-03 13:36:09 +1100

    Altered Preprocess by taking away sample code

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,4 @@
-from pathlib import Path
 import torch
-import matplotlib.pyplot as plt
-plt.rcParams["savefig.bbox"] = 'tight'
-
 from torchvision.transforms import v2
 from torchvision.io import read_image
 
-torch.manual_seed(1)
-
-# If you're trying to run that on collab, you can download the assets and the
-# helpers from https://github.com/pytorch/vision/tree/main/gallery/
-from helpers import plot
-img = read_image(str(Path('../assets') / 'astronaut.jpg'))
-print(f"{type(img) = }, {img.dtype = }, {img.shape = }")
