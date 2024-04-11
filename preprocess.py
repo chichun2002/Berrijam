15b7ae679d39b0b6651b4615b4b07e551afdceb2
Dionne So <you@example.com>
2024-04-11 15:29:12 +1000

    Fixed generate_data to output list of code instead of lists of lists

--- preprocess.py
+++ preprocess.py
@@ -26,7 +26,19 @@ def rotation(image):
     '''
     rotater = v2.RandomRotation(degrees=(0, 180))
     rotated_images = [rotater(image) for _ in range(10)]
-    print(len(rotated_images))
+    return rotated_images
+
+def rotation_multi(images):
+    '''
+    Created 10 random rotations on a single image
+    '''
+    try:
+        rotated_images = []
+        for image in images:
+            rotater = v2.RandomRotation(degrees=(0, 180))
+            rotated_images += [rotater(image) for _ in range(10)]
+    except:
+        rotated_images = rotation(images)
     return rotated_images
 
 def pad(image):
@@ -60,16 +72,44 @@ def blur(image):
     blurred_images = [blurrer(image) for _ in range(10)]
     return blurred_images 
 
+def elasticTransform(image):
+    elastic_transformer = v2.ElasticTransform(alpha=250.0)
+    return [elastic_transformer(image) for _ in range(10)]
+
+def colourJitter(image):
+    jitter = v2.ColorJitter(brightness=.5, hue=.3)
+    return [jitter(image) for _ in range(10)]
+
+def randomInvert(image):
+    inverter = v2.RandomInvert()
+    return [inverter(image) for _ in range(4)]
+
+def randomPosterize(image):
+    posterizer = v2.RandomPosterize(bits=2)
+    return [posterizer(image) for _ in range(4)]
+
+def randomSolarize(image):
+    solarizer = v2.RandomSolarize(threshold=10.0)
+    return [solarizer(image) for _ in range(4)]
+
+
 def generate_data(image):
     '''
     Generate augmented data from single image and return list of images with corresponding list of labels
     '''
+    # convert_tensor = v2.ToTensor()
+    # image = convert_tensor(image)
     expanded = []
     expanded += rotation(image)
-    expanded += pad(image)
-    expanded += pers(image)
-    expanded += aff(image)
-    expanded += blur(image)
+    # for i in expanded[1:10]:
+    #     expanded += pad(i)
+    
+    expanded.extend(pad(image))
+    expanded.extend(pers(image))
+    expanded.extend(aff(image))
+    expanded.extend(blur(image))
+
+    # expanded += randomSolarize(image)
     
     return expanded
 
@@ -87,5 +127,9 @@ if __name__ == '__main__':
     image_file_path = os.path.join(sys.argv[1], sys.argv[2])
     image = load_single_image(image_file_path)
     aug_data = generate_data(image)
-    for i in aug_data:
-        i.show()
+    print(len(aug_data))
+    # aug_data[len(aug_data) - 1].show()
+    # for i in aug_data[len(aug_data) - 1]:
+    #     i.show()
+    # im = aug_data[2].convert["RGB"]
+    # im.show()
