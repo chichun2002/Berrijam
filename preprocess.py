957f507785ea8b36e811a9c055bd68d961490025
Beyond555 <1956308821@qq.com>
2024-04-14 15:25:42 +1000

    Unified all transform functions to allow lists of images being passed into all transformation functions.

--- preprocess.py
+++ preprocess.py
@@ -20,77 +20,134 @@ def ran_number(length):
         list.append(random.randint(1,100))
     return list
 
-def rotation(image):
+def rotation(images):
     '''
-    Created 10 random rotations on a single image
+    Created 10 random rotations from a single or list of image
     '''
     rotater = v2.RandomRotation(degrees=(0, 180))
-    rotated_images = [rotater(image) for _ in range(10)]
-    return rotated_images
-
-def rotation_multi(images):
-    '''
-    Created 10 random rotations on a single image
-    '''
     try:
         rotated_images = []
         for image in images:
-            rotater = v2.RandomRotation(degrees=(0, 180))
-            rotated_images += [rotater(image) for _ in range(10)]
+            rotated_images.extend([rotater(image) for _ in range(10)])
     except:
-        rotated_images = rotation(images)
+        rotated_images = [rotater(image) for _ in range(10)]
     return rotated_images
 
-def pad(image):
+def pad(images):
     '''
-    Created 10 randomly padded image
+    Created 10 randomly padded image from a single or list of image
     '''
-    padded_images = [v2.Pad(padding=padding)(image) for padding in ran_number(10)]
+    try:
+        padded_images = []
+        for image in images:
+            padded_images.extend([v2.Pad(padding=padding)(image) for padding in ran_number(10)])
+    except:
+        padded_images = [v2.Pad(padding=padding)(image) for padding in ran_number(10)]
     return padded_images
 
-def pers(image):
+def pers(images):
     '''
-    Creates 10 random perspectives
+    Creates 10 random perspectives from a single or list of image
     '''
     perspective_transformer = v2.RandomPerspective(distortion_scale=0.5, p=1.0)
-    perspective_images = [perspective_transformer(image) for _ in range(10)]
+    try:
+        perspective_images = []
+        for image in images:
+            perspective_images.extend([perspective_transformer(image) for _ in range(10)])
+    except:
+        perspective_images = [perspective_transformer(image) for _ in range(10)]
     return perspective_images
 
-def aff(image):
+def aff(images):
     '''
-    Creates 10 random affine transformations
+    Creates 10 random affine transformations from a single or list of image
     '''
     affine_transfomer = v2.RandomAffine(degrees=(30, 70), translate=(0.1, 0.3), scale=(0.5, 0.75))
-    affine_images = [affine_transfomer(image) for _ in range(10)]
+    try:
+        affine_images = []
+        for image in images:
+            affine_images.extend([affine_transfomer(image) for _ in range(10)])
+    except:
+        affine_images = [affine_transfomer(image) for _ in range(10)]
     return affine_images
 
-def blur(image):
+def blur(images):
     '''
-    Creates 10 gaussian blurs of original image
+    Creates 10 gaussian blurs of original image from a single or list of image
     '''
     blurrer = v2.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5.))
-    blurred_images = [blurrer(image) for _ in range(10)]
+    try:
+        blurred_images = []
+        for image in images:
+            blurred_images.extend([blurrer(image) for _ in range(10)])
+    except:
+        blurred_images = [blurrer(image) for _ in range(10)]
     return blurred_images 
 
-def elasticTransform(image):
+def elasticTransform(images):
+    '''
+    Creates 10 elastic transforms from a single or list of image
+    '''
     elastic_transformer = v2.ElasticTransform(alpha=250.0)
-    return [elastic_transformer(image) for _ in range(10)]
+    try: 
+        elastic_images = []
+        for image in images:
+            elastic_images.extend([elastic_transformer(image) for _ in range(10)])
+    except:
+        elastic_images = [elastic_transformer(image) for _ in range(10)]
+    return elastic_images
 
-def colourJitter(image):
+def colourJitter(images):
+    '''
+    Creates 10 colour jitters from a single or list of image
+    '''
     jitter = v2.ColorJitter(brightness=.5, hue=.3)
-    return [jitter(image) for _ in range(10)]
+    try:
+        jitter_images = []
+        for image in images:
+            jitter_images.extend([jitter(image) for _ in range(10)])
+    except:
+        jitter_images = [jitter(image) for _ in range(10)]
+    return jitter_images
 
-def randomInvert(image):
+def randomInvert(images):
+    '''
+    Creates 10 random inverts from a single or list of image
+    '''
     inverter = v2.RandomInvert()
-    return [inverter(image) for _ in range(4)]
+    try:
+        inverter_images = []
+        for image in images:
+            inverter_images.extend([inverter(image) for _ in range(10)])
+    except:
+        inverter_images = [inverter(image) for _ in range(10)]
+    return inverter_images
 
-def randomPosterize(image):
+def randomPosterize(images):
+    '''
+    Creates 10 random posterize from a single or list of image
+    '''
     posterizer = v2.RandomPosterize(bits=2)
-    return [posterizer(image) for _ in range(4)]
+    try:
+        posterize_images = []
+        for image in images:
+            posterize_images.extend([posterizer(image) for _ in range(10)])
+    except:
+        posterize_images = [posterizer(image) for _ in range(10)]
+    return posterize_images
 
-def randomSolarize(image):
+def randomSolarize(images):
+    '''
+    Creates 10 random solarize from a single or list of image
+    '''
     solarizer = v2.RandomSolarize(threshold=10.0)
-    return [solarizer(image) for _ in range(4)]
+    try:
+        solarize_images = []
+        for image in images:
+            solarize_images.extend([solarizer(image) for _ in range(10)])
+    except:
+        solarize_images = [solarizer(image) for _ in range(10)]
+    return solarize_images
 
 def grayscale(images):
     '''
@@ -104,8 +161,6 @@ def grayscale(images):
     except:
         random_Gray_images = gray(images)
     return random_Gray_images
-
-
 
 def generate_data(image):
     '''
