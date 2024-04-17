cbe1cea187842c887c2b2e3e7d451bca4ab39dbc
chichun2002 <chichun2002@gmail.com>
2024-04-17 21:44:12 +1000

    Submission Code

---
+++ download_pretrain.py
@@ -0,0 +1,14 @@
+import torchvision
+import torch
+
+model_mnasnet = torchvision.models.mnasnet0_75(weights='IMAGENET1K_V1')
+model_efficientnet = torchvision.models.efficientnet_b0(weights='IMAGENET1K_V1')
+model_shufflenet = torchvision.models.shufflenet_v2_x1_5(weights='IMAGENET1K_V1')
+model_regnet = torchvision.models.regnet_y_400mf(weights='IMAGENET1K_V1')
+model_vit = torchvision.models.vit_b_16(weights='IMAGENET1K_V1')
+
+torch.save(model_mnasnet, 'Pretrained_Models/mnasnet')
+torch.save(model_efficientnet, 'Pretrained_Models/efficientnet')
+torch.save(model_shufflenet, 'Pretrained_Models/shufflenet')
+torch.save(model_regnet, 'Pretrained_Models/regnet')
+torch.save(model_vit, 'Pretrained_Models/vit_b')
